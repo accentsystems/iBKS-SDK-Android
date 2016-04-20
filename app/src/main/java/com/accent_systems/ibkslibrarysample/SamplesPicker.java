43fd8e5bc0c0ca2e21ba3578f89dfed6aff771be
AccentSystems <gcodarcea@accent-systems.com>
2016-04-20 10:39:32 +0200

    First upload

---
+++ app/src/main/java/com/accent_systems/ibkslibrarysample/SamplesPicker.java
@@ -0,0 +1,13 @@
+package com.accent_systems.ibkslibrarysample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SamplesPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samples_picker);
+    }
+}
